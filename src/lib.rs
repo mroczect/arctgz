58b21ed3c7948118d51153cc12052f85b3293270
mroczect <mroczect@proton.me>
2026-07-19 02:24:29 +0700

    feat(lib): export verify function

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub mod handler;
 
 pub use core::{
     compile, execute_recipe, extract, extract_recipe, init, load_config, load_recipe, save_config,
+    verify,
 };
 pub use handler::{ArctgzConfig, ArctgzError, ArctgzManifest, ArctgzRecipe, FileEntry, RecipeStep};
