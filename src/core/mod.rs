3f6c94dfb16649197724f142648b7140ac537dfa
mroczect <mroczect@proton.me>
2026-07-19 01:28:15 +0700

    feat(arctgz): add compile and extract modules

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,5 +1,9 @@
+pub mod compile;
 pub mod config;
+pub mod extract;
 pub mod init;
 
+pub use compile::compile;
 pub use config::{load_config, save_config};
+pub use extract::extract;
 pub use init::init;
