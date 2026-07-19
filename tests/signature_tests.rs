69db8868f43f48920e14359e473b0138087c3796
mroczect <mroczect@proton.me>
2026-07-20 01:24:04 +0700

    test(signature): add password param

--- tests/signature_tests.rs
+++ tests/signature_tests.rs
@@ -20,12 +20,12 @@ fn sign_and_verify() {
         let signing_key = SigningKey::generate(&mut csprng);
         let verifying_key = signing_key.verifying_key();
 
-        let archive = compile(&project, None, false, Some(signing_key.as_bytes())).unwrap();
+        let archive = compile(&project, None, false, Some(signing_key.as_bytes()), None).unwrap();
 
-        verify(&archive, Some(verifying_key.as_bytes())).unwrap();
+        verify(&archive, Some(verifying_key.as_bytes()), None).unwrap();
 
         let out = home.join("out");
-        extract(&archive, &out, false, Some(verifying_key.as_bytes())).unwrap();
+        extract(&archive, &out, false, Some(verifying_key.as_bytes()), None).unwrap();
         assert_eq!(fs::read_to_string(out.join("data.txt")).unwrap(), "secret");
     });
 }
@@ -42,9 +42,9 @@ fn sign_and_verify_wrong_key_fails() {
         let signing_key = SigningKey::generate(&mut csprng);
         let wrong_key = SigningKey::generate(&mut csprng).verifying_key();
 
-        let archive = compile(&project, None, false, Some(signing_key.as_bytes())).unwrap();
+        let archive = compile(&project, None, false, Some(signing_key.as_bytes()), None).unwrap();
 
-        let res = verify(&archive, Some(wrong_key.as_bytes()));
+        let res = verify(&archive, Some(wrong_key.as_bytes()), None);
         assert!(matches!(res, Err(ArctgzError::SignatureError(_))));
     });
 }
@@ -54,13 +54,13 @@ fn unsigned_archive_verify_with_public_key_fails() {
     with_temp_home(|home| {
         let project = home.join("unsigned");
         init(&project, false).unwrap();
-        let archive = compile(&project, None, false, None).unwrap();
+        let archive = compile(&project, None, false, None, None).unwrap();
 
         let mut csprng = OsRng;
         let signing_key = SigningKey::generate(&mut csprng);
         let vk = signing_key.verifying_key();
 
-        let res = verify(&archive, Some(vk.as_bytes()));
+        let res = verify(&archive, Some(vk.as_bytes()), None);
         assert!(matches!(res, Err(ArctgzError::SignatureError(_))));
     });
 }
